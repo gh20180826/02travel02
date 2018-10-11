e643b41abaa341ddd3aae5c55a42627c0fadf88e
Greg <gh.2018.08.26@outlook.com>
2018-10-11 15:13:51 -0500

    made early gulpfile and gulp watch task

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch');
+
+
+
+gulp.task('default', function() {
+     console.log("THIS IS THE DEFAULT TASK!!!.");
+});
+
+gulp.task('html', function() {
+     console.log("THIS IS THE HTML TASK...");
+});
+
+gulp.task('styles', function() {
+     console.log("THIS IS THE STYLES TASK...");
+});
+
+gulp.task('watch', function() {
+
+     watch('./app/index.html', function() {
+          gulp.start('html');
+     });
+
+     watch('./app/assets/styles/**/*.css', function() {
+          gulp.start('styles');
+     });
+
+});
