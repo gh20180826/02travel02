f2ec57729703cf34e30750fdd6640a5171c4e285
Greg <gh.2018.08.26@outlook.com>
2018-10-11 16:56:40 -0500

    auto

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
      return gulp.src('./app/assets/styles/styles.css')
-          .pipe(postcss(cssImport, [cssvars, nested, autoprefixer]))
+          .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
           .pipe(gulp.dest('./app/temp/styles'));
 });
 
